7650a53ba9a420eb2f262db0ac5c7edcc294d016
gayashan4lk <gayashan4lk@gmail.com>
2023-12-07 02:53:52 +0530

    Add react-hook-form dependency and create post function

--- src/pages/posts/new/index.tsx
+++ src/pages/posts/new/index.tsx
@@ -1,6 +1,24 @@
+import axios from 'axios';
 import Link from 'next/link';
 
+import { Post } from '@/types/types';
+
 export default function Page() {
+  async function createPost(post: Post) {
+    const postData = {
+      ...post,
+      userId: 1,
+    };
+
+    const { data } = await axios.post(
+      'https://jsonplaceholder.typicode.com/posts',
+      postData,
+      {
+        headers: { 'Content-type': 'application/json; charset=UTF-8' },
+      }
+    );
+  }
+
   return (
     <>
       <div>Create New Post</div>
